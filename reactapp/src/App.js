41a0e341c95433b0c32457a1070fd3b8afe2ca75
Archives <archives@Archivess-MBP.domain.name>
2020-03-14 19:05:10 +0000

    basic react layout done

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Home from "./components/Home";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  BrowserRouter,
+  Route
+} from 'react-router-dom';
+import './App.css';
+
+class App extends Component {
+
+    state = {
+        userProfile: {},
+        users: []
+    };
+
+    handleUserSelection = (data) => {
+            this.setState({
+              userProfile: {
+                firstName: data["first_name"],
+                secondName: data["last_name"]
+            },
+        })
+      };
+
+    handleGetUserSelection = (data) => {
+        this.setState({
+            users: []
+        })
+    };
+
+    render () {
+        return (
+            <BrowserRouter className="App">
+                <div className="App">
+                    {/* This is where state is defined */}
+                    {/*userProfile = {this.state.userProfile}*/}
+                    {/*  This is where the routes are defined  */}
+                    <Route exact path="/" component={Home}/>
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
---
+++ reactapp/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../css/BasicContainer.css'
+
+const Home = (props) => (
+	  <div className="myDiv">
+		  <h1>Ticket To Ride</h1>
+          <h1>Calculator</h1>
+          <p>By Maxwell Flitton</p>
+	  </div>
+
+    );
+
+export default Home
