bb06c7eb1e9326920ee82d0185d895be61799622
Archives <archives@Archivess-MBP.domain.name>
2020-03-15 13:32:25 +0000

    deployment working

---
+++ expressapp/factory.js
@@ -0,0 +1,1 @@
+// placeholder for now but will route the imports for the express app in the future
--- main.js
+++ main.js
@@ -2,24 +2,21 @@ const {app, BrowserWindow} = require('electron');
 const express = require('express');
 const path = require("path");
 const bodyParser = require('body-parser');
-const {exec} = require('child_process');
-// https://dev.to/jsmanifest/create-your-first-react-desktop-application-in-electron-with-hot-reload-4jj5
 const engine = express();
 engine.use(bodyParser.urlencoded({extended: true}));
-exec('cd reactapp && npm start');
 
+
+engine.use(express.static(path.join(__dirname, "reactapp", 'build')));
 
 engine.get('/', function(req,res) {
-  res.sendFile(path.dirname(__filename) + '/views/index.html');
+    res.sendFile(path.join(__dirname, "reactapp", 'build', 'index.html'));
 });
 
 
 function createWindow () {
-    window = new BrowserWindow({width: 800, height: 600});
-
-    window.loadURL('http://localhost:3000/');
-    // window.loadFile('./views/index.html');
     var server = engine.listen(3001, function() {});
+    window = new BrowserWindow({width: 800, height: 600});
+    window.loadURL("http://127.0.0.1:3001/");
 }
 
 app.on('ready', createWindow);
